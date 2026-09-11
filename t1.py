d7c2c5bf637a37b76ca2a2f1824cfa3946c9341b
number_001 <sungjh.com@gmail.com>
2026-09-11 11:28:50 +0900

    Update t1.py

--- t1.py
+++ t1.py
@@ -12,14 +12,14 @@ dic = {
         "Sex": ["male", "male", "female"],
     }
 
-# df = pd.DataFrame(dic)
-# print(df)
+df = pd.DataFrame(dic)
+print(df)
 
-# df.to_csv("202617342/t1.csv", index=False)
-# print("\n")
+df.to_csv("t1.csv", index=False)
+print("\n")
 
-# a = pd.read_csv("202617342/t1.csv")
-# # print(a)
+a = pd.read_csv("t1.csv")
+# print(a)
 
 # my_list = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]]
 # b = pd.DataFrame(my_list, columns=["A", "B", "C", "D", "E"])

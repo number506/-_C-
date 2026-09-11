2fb8c3e20e58b356a0a056b687442d871b6f2f78
number_001 <sungjh.com@gmail.com>
2026-09-11 11:28:59 +0900

    Update t2.py

--- t2.py
+++ t2.py
@@ -6,5 +6,52 @@ df = pd.read_csv(url)
 print(df)
 
 df.to_csv("t2.csv", index=False)
-# titanic = pd.read_csv("202617342/t2.csv")
-# print(titanic.head())
+titanic = pd.read_csv("-_C-/t2.csv")
+print(titanic.head(8))
+
+titanic.dtypes
+titanic.info()
+
+ages = titanic["Age"]
+
+ages.head()
+
+type(titanic["Age"])
+pd.Series
+
+titanic["Age"].shape
+
+age_sex = titanic[["Age", "Sex"]]
+
+age_sex.head()
+
+type(titanic[["Age", "Sex"]])
+titanic[["Age", "Sex"]].shape
+
+above_35 = titanic[titanic["Age"] > 35]
+
+above_35.head()
+
+titanic["Age"] > 35
+
+above_35.shape
+
+class_23 = titanic[titanic["Pclass"].isin([2, 3])]
+
+class_23.head()
+
+age_no_na = titanic[titanic["Age"].notna()]
+
+age_no_na.head()
+
+age_no_na.shape
+
+adult_names = titanic.loc[titanic["Age"] > 35, "Name"]
+
+adult_names.head()
+
+titanic.iloc[9:25, 2:5]
+
+titanic.iloc[0:3, 3] = "anonymous"
+
+titanic.iloc[:5, 3]

0e5ae3522c0356f5584066adbd9c4511c2c43755
number_001 <sungjh.com@gmail.com>
2026-09-11 09:43:35 +0900

    Update t1.py

--- 202617342/t1.py
+++ 202617342/t1.py
@@ -12,17 +12,23 @@ dic = {
         "Sex": ["male", "male", "female"],
     }
 
+# df = pd.DataFrame(dic)
+# print(df)
+
+# df.to_csv("202617342/t1.csv", index=False)
+# print("\n")
+
+# a = pd.read_csv("202617342/t1.csv")
+# # print(a)
+
+# my_list = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]]
+# b = pd.DataFrame(my_list, columns=["A", "B", "C", "D", "E"])
+# print(b)
+
 df = pd.DataFrame(dic)
-print(df)
+# print(df["Name"])
 
-df.to_csv("202617342/t1.csv", index=False)
-print("\n")
-
-a = pd.read_csv("202617342/t1.csv")
-# print(a)
-
-my_list = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]]
-b = pd.DataFrame(my_list, columns=["A", "B", "C", "D", "E"])
-print(b)
+ages = pd.Series(df["Age"], name="Age")
+print(ages)
 
 print("\n")
